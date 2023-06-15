69ad027a83b8e4c9904b050acb041bd523f2e636
luis.resendiz <luis.resendiz@digitex.ltd>
2023-06-15 00:47:32 -0600

    tests updated: concat

--- src/array/concat.test.ts
+++ src/array/concat.test.ts
@@ -4,3 +4,15 @@ test("array, 2, [3], [[4]]", () => {
   var array = [1];
   expect(concat(array, 2, [3], [[4]])).toStrictEqual([1, 2, 3, [4]]);
 });
+
+test("array, null, undefined, false, '', 0", () => {
+  var array = [1];
+  expect(concat(array, null, undefined, false, "", 0)).toStrictEqual([
+    1,
+    null,
+    undefined,
+    false,
+    "",
+    0,
+  ]);
+});
